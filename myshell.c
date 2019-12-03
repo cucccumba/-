f297b54b8f3500826830051f615dc4354ec97955
CUCCCUMBA <55094588+cucccumba@users.noreply.github.com>
2019-12-03 23:53:52 +0300

    Add files via upload

--- myshell.c
+++ myshell.c
@@ -1,109 +1,104 @@
-#include <stdio.h>
+#include "stdio.h"
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
-int main()
+#define arg_num 24
+#define str_size 216
+#define com_num 24
+
+struct arg
 {
-    int lenght = 0;
-    char string[100] = {};
-    int words_count = 0;
-    printf("cucccumba-GL62M-7REX& ");
-    while (fgets(string, 100, stdin) != NULL)
+    char *command;
+    char *com_arg[arg_num];
+};
+
+int str_parser(struct arg *arg, char *str)
+{
+    int is_pipe = 0;
+    char *p = strchr(str, '|');
+    if (p)
+        is_pipe = 1;
+    if (is_pipe)
     {
-        lenght = strlen(string);
-        string[lenght - 1] = '\0';
-        if (strchr(string, '|'))
+        int cmd_num = 0;
+        int i = 0;
+        char *lines[com_num] = {};
+        for (lines[i] = strtok(str, "|"); lines[i]; ++i, lines[i] = strtok(NULL, "|"))
+            ++cmd_num;
+        for (int j = 0; j < cmd_num; ++j)
         {
-            int comand_count = 0;
-            char *comands[20];
+            int k = 0;
+            for (arg[j].com_arg[k] = strtok(lines[j], " "); arg[j].com_arg[k]; ++k, arg[j].com_arg[k] = strtok(NULL, " "));
+            arg[j].command = arg[j].com_arg[0];
+        }
+        return cmd_num;
+    }
+    else
+    {
 
-            int i = 0;
-            for (comands[i] = strtok(string, "|"); comands[i]; ++i, comands[i] = strtok(NULL, "|"))
+        int k = 0;
+        for (arg[0].com_arg[k] = strtok(str, " "); arg[0].com_arg[k]; ++k, arg[0].com_arg[k] = strtok(NULL, " "));
+        arg[0].command = arg[0].com_arg[0];
+        return 1;
+    }
+}
+
+void Exec (struct arg *arg, int cmd_num)
+{
+    int fd[2];
+    int in_fd = -1;
+    int is_first = 1;
+    int i = 0;
+    for (int j = 0; j < cmd_num; ++j)
+    {
+        if (!is_first)
+            ++i;
+        if (pipe(fd) < 0)
+            perror("ERROR in pipe");
+        if (fork() == 0)
+        {
+            close(fd[0]);
+            if (!is_first)
             {
-                ++comand_count;
+                close(0);
+                dup(in_fd);
+                close(in_fd);
             }
-
-            char *words[20][20] = {}; //[number of program][arguments]
-
-            for (int j = 0; j < comand_count; ++j)
+            if (j != cmd_num - 1)
             {
-                i = 0;
-                for (words[j][i] = strtok(comands[j], " "); words[j][i]; ++i, words[j][i] = strtok(NULL, " "))
-                {
-                }
+                close(1);
+                dup(fd[1]);
+                close(fd[1]);
             }
-
-
-            pid_t pid = 0;
-            int count_pipes = comand_count - 1;
-
-            int fd[2 * count_pipes];
-            for (int i = 0; i < count_pipes; ++i)
-            {
-                if (pipe(fd + 2 * i) < 0)
-                    perror("ERROR IN PIPE");
-            }
-
-            for (int i = 0, j = 1; i < comand_count; ++i, j += 2) // j - counter of output fd
-            {
-                int prev_fd = 0;
-                pid = fork();
-                if (pid == 0)
-                {
-                    if (i == 0) // first command
-                    {
-                        close (1);
-                        if (dup(fd[j]) < 0) 
-                            perror("ERROR IN DUP i == 0");
-                        prev_fd = j - 1; // input for pipe in next process
-                    }
-                    else if (i > 0 && i < comand_count - 1) // between first and last command
-                    {
-                        close (0);
-                        if (dup(fd[prev_fd]) < 0)
-                            perror("ERROR IN DUP i > 0 (0)");
-
-                        close (1);
-                        if(dup(fd[j]) < 0)
-                            perror("ERROR IN DUP i > 0 (1)");
-                        prev_fd = j - 1; // input for pipe in next process
-                    }
-                    else if (i == comand_count - 1) // last command
-                    {
-                        close (0);
-                        if (dup(fd[prev_fd]) < 0)
-                            perror("ERROR IN DUP i == comand_count - 1 (0)");
-                    }
-                    for (int i = 0; i < 2 * count_pipes; ++i)
-                        close(fd[i]);
-
-                    if (execvp(words[i][0], words[i]) < 0)
-                        perror("ERROR IN EXEC");
-                }
-            }
-            for (int i = 0; i < count_pipes * 2; ++i) // closes fd in parent
-                close(fd[i]);
-            for (int i = 0; i < comand_count + 1; ++i) // waits for all children
-                wait(NULL);
-            printf("cucccumba-GL62M-7REX& ");
+            execvp(arg[i].command, arg[i].com_arg);
         }
         else
         {
-            char *words[20];
-            int i = 0;
-            for (words[i] = strtok(string, " "); words[i]; ++i, words[i] = strtok(NULL, " "))
-                ++words_count;
-            pid_t pid = fork();
-            if (pid == 0)
-            {
-                words[words_count] = NULL;
-                execvp(words[0], words);
-            }
-            wait(NULL);
-            printf("cucccumba-GL62M-7REX& ");
+            if (in_fd != -1)
+                close(in_fd);
+            in_fd = fd[0];
+            close(fd[1]);
         }
+        is_first = 0;
+    }
+
+}
+
+int main()
+{
+    char str[str_size] = {};
+    printf("cucccumba-GL62M-7REX& ");
+    while (fgets(str, str_size, stdin) != NULL)
+    {
+        struct arg arg[com_num];
+        *(str + strlen(str) - 1) = '\0';
+        int cmd_num = str_parser(arg, str);
+        Exec(arg, cmd_num);
+        for (int i = 0; i < cmd_num; ++i)
+            wait(NULL);
+        printf("cucccumba-GL62M-7REX& ");
     }
     return 0;
 }
